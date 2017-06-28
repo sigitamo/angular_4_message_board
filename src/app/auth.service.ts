ba471bb3219908abb8c671501406477e7801f6ce
sigitamocevice <sigita.z@gmail.com>
2017-06-28 10:49:32 +0300

    button welcome for authenticated user

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Router } from '@angular/router'; 
 
 @Injectable()
 export class AuthService {
     
     BASE_URL = 'http://localhost:63145/auth';
+    NAME_KEY = 'name';
+    TOKEN_KEY = 'token';
     
-    constructor(private http: Http) {}
+    constructor(private http: Http, private router: Router) {}
 
-   register(user) {
-    delete user.confirmPassword; 
-    this.http.post(this.BASE_URL + '/register', user).subscribe(res => {
-        localStorage.setItem('token', res.json().token)
-        localStorage.setItem('name ', res.json().firstName)
-    });
+    get name() { 
+        return localStorage.getItem(this. NAME_KEY); 
+    }
+    
+    get isAuthenticated() {
+        return !!localStorage.getItem(this.TOKEN_KEY); 
+    }
+    register(user) {
+        delete user.confirmPassword; 
+        this.http.post(this.BASE_URL + '/register', user).subscribe(res => {
+            
+            var authResponse = res.json();
+
+            if(!authResponse.token)
+            return;
+
+            localStorage.setItem(this.TOKEN_KEY, authResponse.token)
+            localStorage.setItem(this.NAME_KEY, authResponse.firstName)
+            this.router.navigate(['/']);
+        });
    } 
 } 
 
--- src/app/nav.component.ts
+++ src/app/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component ({
     selector: 'nav',
@@ -6,11 +7,13 @@ import { Component } from '@angular/core';
      <div class="nav-bar" style="background-color: blue; padding: 10px;">
         <button routerLink="/" class=btn-primary>Message BOARD</button>
         <button routerLink="/messages" class=btn-primary>Messages</button>
+        <span style="flex:1 1 auto"></span>
         <button routerLink="/register" class=btn-primary>Register</button>
+        <button *ngIf="auth.isAuthenticated" routerLink="/register" class=btn-primary>Welcome {{auth.name }}</button> 
     </div>
     ` 
 })
 
  export class NavComponent {
-       constructor() {}
+       constructor(private auth: AuthService) {}
 }
